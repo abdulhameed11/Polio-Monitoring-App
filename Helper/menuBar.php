e7bc8ed039fa3580b37595a26e448dacb7c76dd9
abdul_hameed <33574581+abdulhameed11@users.noreply.github.com>
2019-03-20 17:39:30 +0500

    commit for menuBar and Sidebar

---
+++ Helper/menuBar.php
@@ -0,0 +1,27 @@
+<nav  class="nav navbar-default navbar-fixed">
+     
+         <div  class="container-fluid"> 
+             <div  class="collapse navbar-collapse">
+                    <ul style="padding-top:0em" class="nav navbar-nav navbar-right in ">
+                    <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                            </a>
+                            <ul class="caret dropdown-menu">
+                                <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                                </li>
+                            </ul>
+                            <!-- /.dropdown-user -->
+                        </li>
+                        <!-- /.dropdown -->
+                    </ul>
+                    </ul>
+                <div>
+            </div>
+        
+        </nav>
---
+++ Helper/sideBar.php
@@ -0,0 +1,96 @@
+
+
+<html>
+<head>
+
+<style>
+    img{
+float:left;
+
+height:1.3em;
+width:1.3em;
+    }
+a>p{
+color:#337ab7;
+padding-left:2em;
+}
+</style>
+
+
+<link href="assets/css/side.css" rel="stylesheet">
+
+  
+
+</head>
+</body>
+
+    
+<div class="sidebar-wrapper  sidebar" >
+    	<div class="sidebar-wrapper"> 
+            <div class="logo">
+                <a href="" class="simple-text">
+                   LOGO
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li class="active">
+                    <a href="/Project/dashboard.php">
+                    <img  src="/Project/assets/img/home.png">
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li >
+                    <a href="/Project/Profile.php">
+                    
+                    <img  src="/Project/assets/img/user.png">
+                    <p style="color:#337ab7">User Profile</p>   
+                    </a>
+                </li>
+                <li>
+                    <a href="crud2/table.php">
+                   
+                    <img  src="/Project/assets/img/list.png">
+                        <p>Volunteer List</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="/Project/Notification.php">
+                    <img  src="/Project/assets/img/bell.png">
+                        <p>Notifications</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="/Project/Event.php">
+                    <img  src="/Project/assets/img/calendar.png">
+                        <p>Event</p>
+                    </a>
+                </li>
+                <li>
+                   
+                </li>
+                <li>
+                    <a href="notifications.html">
+                       
+                        <p></p>
+                    </a>
+                </li>
+				<li class="active-pro">
+                    <a href="upgrade.html">
+                        
+                        <p></p>
+                    </a>
+                </li>
+            </ul>
+    	</div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
